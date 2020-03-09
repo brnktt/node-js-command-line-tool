17780b85705ed2bed7bd7ac4c58699f90956213e
Branchetti Alessandro <40342541+brnktt@users.noreply.github.com>
2020-03-09 19:54:54 +0100

    Ls function reproduction with Promisses

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
-const fs = require("fs");
+#!/usr/bin/env node
 
-fs.readdir(process.cwd(), (err, filenames) => {
+const fs = require("fs");
+const util = require("util");
+const chalk = require("chalk");
+const path = require("path");
+
+// Method #2
+// const lstat = util.promisify(fs.lstat);
+
+// Method #3
+const { lstat } = fs.promises;
+
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, filenames) => {
   // err === an error object, which means something went wrong
   // err === nell, which means everything is OK
-
   if (err) {
-    //throw new Error(err);
     console.log(err);
   }
 
-  console.log(filenames);
+  const statPromises = filenames.map(filename => {
+    return lstat(path.join(targetDir, filename));
+  });
+
+  const allStats = await Promise.all(statPromises);
+
+  for (let stats of allStats) {
+    const index = allStats.indexOf(stats);
+
+    if (stats.isFile()) {
+      console.log(filenames[index]);
+    } else {
+      console.log(chalk.bold(filenames[index]));
+    }
+  }
 });
+
+// Method #1
+// const lstat = filename => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if (err) {
+//         reject(err);
+//       }
+
+//       resolve(stats);
+//     });
+//   });
+// };
